fba63c06cf1c961e068d70e90123d73c8464869e
Ala404 <alaeddine.kezzouz@univ-constantine2.dz>
2023-06-08 08:01:28 +0100

    fixing points register style

--- resources/js/src/components/student_components/points/PointsRegister.vue
+++ resources/js/src/components/student_components/points/PointsRegister.vue
@@ -1,8 +1,7 @@
 <template>
-    
-      <table class="w-full border-gray-300 border-2 table-auto mx-auto ">
-        <thead>
-        
+
+        <!-- <thead>
+
           <tr class="bg-slate-100">
             <th class="border-gray-300 border-2 px-4 py-2"></th>
             <th class="border-gray-300 border-2 px-4 py-2 text-center bg-gray-300">Excellent</th>
@@ -11,73 +10,87 @@
             <th class="border-gray-300 border-2 px-4 py-2 text-center bg-gray-300">Insuffisant</th>
             <th class="border-gray-300 border-2 px-4 py-2 text-center bg-gray-300">Very Week</th>
           </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th class="border-gray-300 border-2 px-4 py-2">General discipline and human relations</th>
+        </thead> -->
+        <section class="flex justify-between items-center w-full flex-wrap bg-transparent">
+          <div class="bg-white py-8 rounded-lg shadow-lg hover:text-white hover:bg-primary cursor-pointer">
+            <!-- point as big number -->
+            <h2 class=" text-center text-4xl font-bold text-secondary ">{{discipline}}</h2>
+            <p class="text-center px-4 py-2 text-xl font-semibold">General discipline and human relations</p>
 
-            <td class="border-gray-300 border-2 px-4 py-2 text-center" v-for="note in notes" >{{ discipline === note? discipline:0 }}</td>
-          </tr>
-          <tr class="bg-slate-100">
-            <th class="border-gray-300 border-2 px-4 py-2">Work skills and handling</th>
+          </div>
+          <div class="bg-white py-8 rounded-lg shadow-lg hover:text-white hover:bg-primary cursor-pointer">
+            <!-- point as big number -->
+            <h2 class="text-4xl text-center font-bold text-secondary">{{attitude}}</h2>
+            <p class="text-center px-4 py-2 text-xl font-semibold">Work skills and handling</p>
 
-            <td class="border-gray-300 border-2 px-4 py-2 text-center" v-for="note in notes" >{{ attitude === note ? attitude : 0 }}</td>
-          </tr>
-          <tr>
-            <th class="border-gray-300 border-2 px-4 py-2">Initiative/Leadership</th>
+          </div>
+          <div class="bg-white py-8 rounded-lg shadow-lg hover:text-white hover:bg-primary cursor-pointer">
 
-            <td class="border-gray-300 border-2 px-4 py-2 text-center" v-for="note in notes" >{{ initiative ===note? initiative:0 }}</td>
-          </tr>
-          <tr class="bg-slate-100">
-            <th class="border-gray-300 border-2 px-4 py-2">Imagination skills and innovation</th>
+            <!-- point as big number -->
+            <h2 class="text-4xl text-center font-bold text-secondary">{{initiative}}</h2>
+            <p class="text-center px-4 py-2 text-xl font-semibold">Initiative/Leadership</p>
 
-            <td class="border-gray-300 border-2 px-4 py-2 text-center" v-for="note in notes" >{{ capacite === note ? capacite:0 }}</td>
-            
-            
-          </tr>
-          <tr>
-            <th class="border-gray-300 border-2 px-4 py-2">Knowledge acquired in the internship field</th>
+          </div>
+          <div class="bg-white py-8 rounded-lg shadow-lg  hover:text-white hover:bg-primary cursor-pointer">
 
-            <td class="border-gray-300 border-2 px-4 py-2 text-center" v-for="note in notes" >{{ connaissance === note ? connaissance:0 }}</td>
+            <!-- point as big number -->
+            <h2 class="text-4xl text-center font-bold text-secondary">{{capacite}}</h2>
+            <p class="text-center px-4 py-2 text-xl font-semibold">Imagination skills and innovation</p>
 
-          </tr>
-          <tr class="bg-primary">
-            <th class="border-gray-300 border-2 px-4 py-2">Total Point</th>
-            <td class="text-center">{{ note_totale }}</td>
-          </tr>
-        </tbody>
-        </table>
-    
+
+
+          </div>
+         <div class="mt-8 flex justify-between w-full">
+            <div class="bg-white py-8 rounded-lg shadow-lg hover:text-white hover:bg-primary cursor-pointer">
+              <!-- point as big number -->
+              <h2 class="text-4xl text-center font-bold text-secondary">{{connaissance}}</h2>
+              <div class="text-center px-4 py-2 text-xl font-semibold">Knowledge acquired in the internship field</div>
+
+
+            </div>
+            <div class="bg-secondary rounded-lg py-8 shadow-lg cursor-pointer hover:text-secondary">
+              <h2 class="text-center font-bold text-3xl text-white ">{{ note_totale }}</h2>
+              <p class="text-center px-4 py-2 text-lg font-medium text-white ">Total Point</p>
+
+            </div>
+         </div>
+        </section>
+
+
 </template>
 
 <script>
 export default {
   name: "PointsRegister",
   props: {
-   
+
       discipline: Number,
       attitude: Number,
       initiative: Number,
       capacite: Number,
       connaissance: Number,
       note_totale: Number,
-    
+
   },
     data() {
         return {
             notes: [4, 3, 2,1 , 0],
-      
-            
+
+
         };
   },
   computed: {
     //calculate the value of each property from  notes
 
   }
-   
+
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+
+
+
 
 </style>
